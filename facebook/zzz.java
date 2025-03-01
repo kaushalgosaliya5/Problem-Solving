3cc9b4e7867f65dc8ab46460be476a25bd8dbdac
kaushalgosaliya5 <kaushalgosaliya5@gmail.com>
2025-03-01 21:16:12 +0530

    add sample java

---
+++ facebook/zzz.java
@@ -0,0 +1,12 @@
+package facebook;
+
+import java.util.*;
+
+public class zzz {
+      public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        
+
+      }    
+}
